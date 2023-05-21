21786259dbd819bd96090e68bcad9b3274b5996f
서동경 <ehhdrud@icloud.com>
2023-05-22 00:02:10 +0900

    230522

--- src/components/headerComponents/Category.js
+++ src/components/headerComponents/Category.js
@@ -1,97 +1,41 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import "../../styles/category.css";
-
-// export default function Category() {
-//   return (
-//     <nav className="category">
-//       <Link className="link" to="/vision">
-//         ⭐Vision
-//       </Link>
-//       <Link className="link" to="/about">
-//         ⭐About
-//       </Link>
-//       <Link className="link" to="/artist">
-//         ⭐Artist
-//       </Link>
-//       <Link className="link" to="/marketplace">
-//         ⭐Marketplace
-//       </Link>
-//       <Link className="link" to="/faq">
-//         ⭐FAQ
-//       </Link>
-//     </nav>
-//   );
-// }
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import "../../styles/category.css";
 
 export default function Category() {
   const location = useLocation();
-  const [visitedLinks, setVisitedLinks] = useState([]);
-
-  useEffect(() => {
-    const visitedLink = localStorage.getItem("visitedLink");
-    if (visitedLink) {
-      setVisitedLinks(JSON.parse(visitedLink));
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("visitedLink", JSON.stringify(visitedLinks));
-  }, [visitedLinks]);
-
-  const handleLinkClick = (link) => {
-    if (!visitedLinks.includes(link)) {
-      setVisitedLinks([...visitedLinks, link]);
-    }
-  };
-
-  useEffect(() => {
-    const updatedVisitedLinks = visitedLinks.filter((link) =>
-      location.pathname.startsWith(link)
-    );
-    setVisitedLinks(updatedVisitedLinks);
-  }, [location.pathname]);
 
   return (
     <nav className="category">
       <Link
-        className={`link ${visitedLinks.includes("/vision") ? "visited" : ""}`}
+        className={`link ${location.pathname === "/vision" ? "visited" : ""}`}
         to="/vision"
-        onClick={() => handleLinkClick("/vision")}
       >
         ⭐Vision
       </Link>
       <Link
-        className={`link ${visitedLinks.includes("/about") ? "visited" : ""}`}
+        className={`link ${location.pathname === "/about" ? "visited" : ""}`}
         to="/about"
-        onClick={() => handleLinkClick("/about")}
       >
         ⭐About
       </Link>
       <Link
-        className={`link ${visitedLinks.includes("/artist") ? "visited" : ""}`}
+        className={`link ${location.pathname === "/artist" ? "visited" : ""}`}
         to="/artist"
-        onClick={() => handleLinkClick("/artist")}
       >
         ⭐Artist
       </Link>
       <Link
         className={`link ${
-          visitedLinks.includes("/marketplace") ? "visited" : ""
+          location.pathname === "/marketplace" ? "visited" : ""
         }`}
         to="/marketplace"
-        onClick={() => handleLinkClick("/marketplace")}
       >
         ⭐Marketplace
       </Link>
       <Link
-        className={`link ${visitedLinks.includes("/faq") ? "visited" : ""}`}
+        className={`link ${location.pathname === "/faq" ? "visited" : ""}`}
         to="/faq"
-        onClick={() => handleLinkClick("/faq")}
       >
         ⭐FAQ
       </Link>

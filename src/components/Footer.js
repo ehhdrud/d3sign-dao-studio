ed75983217252fdba43be48923cdbcd938bca60e
서동경 <ehhdrud@icloud.com>
2023-06-11 16:10:47 +0900

    230611-Footer레이아웃2차변경

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { ToggleContext } from "../ToggleContext";
 import Sns from "./Sns";
 import "../styles/footer.css";
+import logo from "../images/logo.jpg";
 
 export default function Footer() {
   const { deleteCategoryLayout, categoryLayoutChanged } =
@@ -16,26 +17,14 @@ export default function Footer() {
       className={`footer ${categoryLayoutChanged ? "rearranged" : ""}`}
       onClick={handleClick}
     >
-      <div className="container copyright">
-        <p className="title">Copyright</p>
-        <p className="text text-main">
+      <div className="logo-and-text">
+        <img className="logo" src={logo} alt="logo" />
+        <p className="text">
           © 2023 D3sign DAO Studio. All rights reserved.
-        </p>
-        <p className="text text-sub">
-          This website and its content are protected by copyright law.
+          <br />
+          Contact us: d3signdaostudio@gmail.com
         </p>
       </div>
-
-      <div className="container contact">
-        <p className="title">Contact</p>
-        <p className="text contact-text">
-          If you have any questions, please contact me via Instagram DM or
-          email.
-        </p>
-        <p className="via instagram">instagram.com/d3sign_dao</p>
-        <p className="via email">d3signdaostudio@gmail.com</p>
-      </div>
-
       <Sns />
     </div>
   );

03b8658be7fbe8f97e30a80d5968fc9455fd9503
서동경 <ehhdrud@icloud.com>
2023-06-14 13:52:12 +0900

    230614-Category모바일환경에서투명공간추가

--- src/components/headerComponents/Category.js
+++ src/components/headerComponents/Category.js
@@ -9,54 +9,57 @@ export default function Category() {
 
   return (
     <nav className={`category ${categoryLayoutChanged ? "rearranged" : ""}`}>
-      <div className="category-item">
-        <Link
-          className={`link ${
-            location.pathname.startsWith("/d3sign-dao-studio/whatson")
-              ? "visited"
-              : ""
-          }`}
-          to="/d3sign-dao-studio/whatson"
-        >
-          <span className="category-text">What's on</span>
-        </Link>
+      <div className="category-items">
+        <div className="category-item">
+          <Link
+            className={`link ${
+              location.pathname.startsWith("/d3sign-dao-studio/whatson")
+                ? "visited"
+                : ""
+            }`}
+            to="/d3sign-dao-studio/whatson"
+          >
+            <span className="category-text">What's on</span>
+          </Link>
+        </div>
+        <div className="category-item">
+          <Link
+            className={`link ${
+              location.pathname.startsWith("/d3sign-dao-studio/artsandartists")
+                ? "visited"
+                : ""
+            }`}
+            to="/d3sign-dao-studio/artsandartists"
+          >
+            <span className="category-text">Arts and artists</span>
+          </Link>
+        </div>
+        <div className="category-item">
+          <Link
+            className={`link ${
+              location.pathname.startsWith("/d3sign-dao-studio/marketplace")
+                ? "visited"
+                : ""
+            }`}
+            to="/d3sign-dao-studio/marketplace"
+          >
+            <span className="category-text">Marketplace</span>
+          </Link>
+        </div>
+        <div className="category-item">
+          <Link
+            className={`link ${
+              location.pathname.startsWith("/d3sign-dao-studio/faq")
+                ? "visited"
+                : ""
+            }`}
+            to="/d3sign-dao-studio/faq"
+          >
+            <span className="category-text">FAQ</span>
+          </Link>
+        </div>
       </div>
-      <div className="category-item">
-        <Link
-          className={`link ${
-            location.pathname.startsWith("/d3sign-dao-studio/artsandartists")
-              ? "visited"
-              : ""
-          }`}
-          to="/d3sign-dao-studio/artsandartists"
-        >
-          <span className="category-text">Arts and artists</span>
-        </Link>
-      </div>
-      <div className="category-item">
-        <Link
-          className={`link ${
-            location.pathname.startsWith("/d3sign-dao-studio/marketplace")
-              ? "visited"
-              : ""
-          }`}
-          to="/d3sign-dao-studio/marketplace"
-        >
-          <span className="category-text">Marketplace</span>
-        </Link>
-      </div>
-      <div className="category-item">
-        <Link
-          className={`link ${
-            location.pathname.startsWith("/d3sign-dao-studio/faq")
-              ? "visited"
-              : ""
-          }`}
-          to="/d3sign-dao-studio/faq"
-        >
-          <span className="category-text">FAQ</span>
-        </Link>
-      </div>
+      <div className="transparent-space"></div>
     </nav>
   );
 }

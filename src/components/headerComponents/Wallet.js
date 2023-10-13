769f25d7c777908c82fd426fa8225e3bffd0f95e
서동경 <ehhdrud@icloud.com>
2023-10-13 15:02:29 +0900

    fix: [main] - Wallet 컴포넌트 warning 수정 및 가독성 개선

--- src/components/headerComponents/Wallet.js
+++ src/components/headerComponents/Wallet.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { ethers } from 'ethers';
 import MetaFoxLogo from './walletComponents/MetaFoxLogo.js';
 import ethereumLogo from '../../images/ethereum-logo.svg';
@@ -16,100 +16,88 @@ export default function Wallet() {
     const provider = new ethers.BrowserProvider(window.ethereum);
 
     // 계정과 현재 네트워크를 가져오는 비동기 함수
-    const getAccountAndNetwork = async () => {
-        // window.ethereum이 존재한다면,
-        if (window.ethereum) {
-            try {
-                // Ethereum Provider API에서 eth_requestAccounts를 통해 계정 정보를 반환받음
-                const accounts = await window.ethereum.request({
-                    method: 'eth_requestAccounts',
-                });
-                console.log('Accounts:', accounts);
-                // 현재 로그인된 계정(index=0)을 상태에 저장
-                setWalletAddress(accounts[0]);
-                console.log('Wallet Address: ', walletAddress);
+    const getAccountAndNetwork = useCallback(async () => {
+        try {
+            // Ethereum Provider API에서 eth_requestAccounts를 통해 계정 정보를 반환받음
+            const accounts = await window.ethereum.request({
+                method: 'eth_requestAccounts',
+            });
+            console.log('Accounts:', accounts);
+            // 현재 로그인된 계정(index=0)을 상태에 저장
+            setWalletAddress(accounts[0]);
+            console.log('Wallet Address: ', walletAddress);
 
-                const addressShortcut = accounts[0].slice(0, 6) + '...' + accounts[0].slice(-4);
-                setShortenedAddress(addressShortcut);
-                // 현재 연결된 Ethereum 네트워크의 식별자를 가져와서 networkId 변수에 저장
-                const networkId = window.ethereum.networkVersion;
-                // 홈페이지에서 사용할 네트워크 이름을 식별자 별로 저장
-                const networkNames = {
-                    1: 'Ethereum Mainnet',
-                    137: 'Polygon Mainnet',
-                };
-                // 해당 network를 사용 중이라면 해당 네트워크의 이름을 띄움
-                const name = networkNames[networkId] || 'Unregistered network';
-                setNetworkName(name);
-                console.log('Current network name: ', name);
-            } catch (error) {
-                // 에러가 뜨면 계정, 네트워크 이름 초기화
-                setWalletAddress('');
-                setNetworkName('');
-                console.log(
-                    "Error at 'getAccountAndNetwork' - 메타마스크 네트워크 연결이 필요합니다. "
-                );
-                console.error(error);
-            }
+            const addressShortcut = accounts[0].slice(0, 6) + '...' + accounts[0].slice(-4);
+            setShortenedAddress(addressShortcut);
+            // 현재 연결된 Ethereum 네트워크의 식별자를 가져와서 networkId 변수에 저장
+            const networkId = window.ethereum.networkVersion;
+            // 홈페이지에서 사용할 네트워크 이름을 식별자 별로 저장
+            const networkNames = {
+                1: 'Ethereum Mainnet',
+                137: 'Polygon Mainnet',
+            };
+            // 해당 network를 사용 중이라면 해당 네트워크의 이름을 띄움
+            const name = networkNames[networkId] || 'Unregistered network';
+            setNetworkName(name);
+            console.log('Current network name: ', name);
+        } catch (error) {
+            // 에러가 뜨면 계정, 네트워크 이름 초기화
+            setWalletAddress('');
+            setNetworkName('');
+            console.log('메타마스크에서 네트워크 연결이 필요합니다. ');
         }
-    };
-
-    const createETHString = (amount) => {
-        return amount + 'ETH';
-    };
-
-    const createMATICString = (amount) => {
-        return amount + 'MATIC';
-    };
+    }, [walletAddress]);
 
     // 보유자산 정보를 가져오는 함수
     const getBalance = async (address) => {
-        try {
-            const balance = await provider.getBalance(address);
-
-            if (networkName === 'Ethereum Mainnet') {
-                const ethString = createETHString(ethers.formatEther(balance));
-                setBalanceString(ethString);
-            } else if (networkName === 'Polygon Mainnet') {
-                const maticString = createMATICString(ethers.formatEther(balance));
-                setBalanceString(maticString);
-            } else {
-                setBalanceString('Unregistered balance');
-            }
-        } catch (error) {
-            console.log("Error at 'getBalance' - 메타마스크 네트워크 연결이 필요합니다.");
-            console.error(error);
-        }
-    };
-
-    // 지갑을 연결하는 함수
-    const connectWallet = async () => {
-        // 만약 window.ethereum이 존재한다면
         if (window.ethereum) {
-            // 계정, 네트워크 정보를 가져오는 함수를 실행
-            await getAccountAndNetwork();
+            const createETHString = (amount) => {
+                return amount + 'ETH';
+            };
 
-            window.ethereum.on('accountsChanged', connectWallet);
-            window.ethereum.on('networkChanged', connectWallet);
+            const createMATICString = (amount) => {
+                return amount + 'MATIC';
+            };
+
+            try {
+                const balance = await provider.getBalance(address);
+
+                if (networkName === 'Ethereum Mainnet') {
+                    const ethString = createETHString(ethers.formatEther(balance));
+                    setBalanceString(ethString);
+                } else if (networkName === 'Polygon Mainnet') {
+                    const maticString = createMATICString(ethers.formatEther(balance));
+                    setBalanceString(maticString);
+                } else {
+                    setBalanceString('Unregistered balance');
+                }
+            } catch (error) {
+                console.log('메타마스크에서 네트워크 연결이 필요합니다.');
+            }
         }
     };
 
     // 마운트 시 실행되는 함수 정의
     useEffect(() => {
-        connectWallet();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    // walletAddress의 상태가 변경되면 실행되는 함수 정의
-    useEffect(() => {
-        getBalance(walletAddress);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [walletAddress, networkName]);
+        if (typeof window.ethereum !== 'undefined') {
+            getAccountAndNetwork();
+            window.ethereum.on('accountsChanged', getAccountAndNetwork);
+            window.ethereum.on('networkChanged', getAccountAndNetwork);
+        } else {
+            console.log('MetaMask가 설치되어 있지 않습니다.');
+        }
+    }, [getAccountAndNetwork]);
 
     return (
         <div>
             <div className="wallet">
-                <button className="wallet-button" onClick={() => setModalState(true)}>
+                <button
+                    className="wallet-button"
+                    onClick={() => {
+                        getBalance(walletAddress);
+                        setModalState(true);
+                    }}
+                >
                     <div className="wallet-button-items">
                         <MetaFoxLogo />
                         {walletAddress && <p>{networkName}</p>}

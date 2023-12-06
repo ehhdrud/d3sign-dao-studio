15c758f7b2331a6c61c8e14cf6f67aa6a19418cd
서동경 <ehhdrud@icloud.com>
2023-12-06 21:46:51 +0900

    refector: [main] - 렌더링 최적화용 작은 크기의 이미지 추가

--- src/components/bodyComponents/Default.js
+++ src/components/bodyComponents/Default.js
@@ -5,16 +5,16 @@ import { SyncLoader } from 'react-spinners';
 import './styles/default.css';
 import { MetasPageDefault } from '../../metadatas/metadatas';
 
-import V1VI from '../../images/arts/V1VI/V1VI-00.webp';
-import P0LA from '../../images/arts/P0LA/P0LA-00.webp';
-import R0RO from '../../images/arts/R0RO/R0RO-00.webp';
-import T0RI from '../../images/arts/T0RI/T0RI-00.webp';
-import CHR1S from '../../images/arts/CHR1S/CHR1S-00.webp';
-import ZER1 from '../../images/arts/ZER1/ZER1-00.webp';
-import J1N from '../../images/arts/J1N/J1N-00.webp';
-import MIRR0R from '../../images/arts/MIRR0R/MIRR0R-00.webp';
-import PAUL0 from '../../images/arts/PAUL0/PAUL0-00.webp';
-import R1N from '../../images/arts/R1N/R1N-00.webp';
+import V1VI from '../../images/arts/resized_img/Resize_V1VI-00.webp';
+import P0LA from '../../images/arts/resized_img/Resize_P0LA-00.webp';
+import R0RO from '../../images/arts/resized_img/Resize_R0RO-00.webp';
+import T0RI from '../../images/arts/resized_img/Resize_T0RI-00.webp';
+import CHR1S from '../../images/arts/resized_img/Resize_CHR1S-00.webp';
+import ZER1 from '../../images/arts/resized_img/Resize_ZER1-00.webp';
+import J1N from '../../images/arts/resized_img/Resize_J1N-00.webp';
+import MIRR0R from '../../images/arts/resized_img/Resize_MIRR0R-00.webp';
+import PAUL0 from '../../images/arts/resized_img/Resize_PAUL0-00.webp';
+import R1N from '../../images/arts/resized_img/Resize_R1N-00.webp';
 
 const imageFiles = [V1VI, P0LA, R0RO, T0RI, CHR1S, ZER1, J1N, MIRR0R, PAUL0, R1N];
 

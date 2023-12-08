a5891c1663b28825c822387b93a06ed660f4e9fc
서동경 <ehhdrud@icloud.com>
2023-12-08 09:41:33 +0900

    refector: [main] - 렌더링 최적화용 모바일 이미지 추가

--- src/components/bodyComponents/Default.js
+++ src/components/bodyComponents/Default.js
@@ -15,57 +15,79 @@ import J1N from '../../images/arts/resized_img/Resize_J1N-00.webp';
 import MIRR0R from '../../images/arts/resized_img/Resize_MIRR0R-00.webp';
 import PAUL0 from '../../images/arts/resized_img/Resize_PAUL0-00.webp';
 import R1N from '../../images/arts/resized_img/Resize_R1N-00.webp';
+import V1VIMobile from '../../images/arts/resized_mobile_img/Resize_Mobile_V1VI-00.webp';
+import P0LAMobile from '../../images/arts/resized_mobile_img/Resize_Mobile_P0LA-00.webp';
+import R0ROMobile from '../../images/arts/resized_mobile_img/Resize_Mobile_R0RO-00.webp';
+import T0RIMobile from '../../images/arts/resized_mobile_img/Resize_Mobile_T0RI-00.webp';
+import CHR1SMobile from '../../images/arts/resized_mobile_img/Resize_Mobile_CHR1S-00.webp';
+import ZER1Mobile from '../../images/arts/resized_mobile_img/Resize_Mobile_ZER1-00.webp';
+import J1NMobile from '../../images/arts/resized_mobile_img/Resize_Mobile_J1N-00.webp';
+import MIRR0RMobile from '../../images/arts/resized_mobile_img/Resize_Mobile_MIRR0R-00.webp';
+import PAUL0Mobile from '../../images/arts/resized_mobile_img/Resize_Mobile_PAUL0-00.webp';
+import R1NMobile from '../../images/arts/resized_mobile_img/Resize_Mobile_R1N-00.webp';
 
 const imageFiles = [V1VI, P0LA, R0RO, T0RI, CHR1S, ZER1, J1N, MIRR0R, PAUL0, R1N];
+const imageFilesMobile = [
+    V1VIMobile,
+    P0LAMobile,
+    R0ROMobile,
+    T0RIMobile,
+    CHR1SMobile,
+    ZER1Mobile,
+    J1NMobile,
+    MIRR0RMobile,
+    PAUL0Mobile,
+    R1NMobile,
+];
 
 const arts = [
     {
-        path: imageFiles[0],
+        path: [imageFiles[0], imageFilesMobile[0]],
         artist: 'V1VI',
         genre: 'Collage Art',
     },
     {
-        path: imageFiles[1],
+        path: [imageFiles[1], imageFilesMobile[1]],
         artist: 'P0LA',
         genre: 'Pop Art',
     },
     {
-        path: imageFiles[2],
+        path: [imageFiles[2], imageFilesMobile[2]],
         artist: 'R0RO',
         genre: 'Pattern Fabric Art',
     },
     {
-        path: imageFiles[3],
+        path: [imageFiles[3], imageFilesMobile[3]],
         artist: 'T0RI',
         genre: 'Anime Art',
     },
     {
-        path: imageFiles[4],
+        path: [imageFiles[4], imageFilesMobile[4]],
         artist: 'CHR1S',
         genre: 'Cubism',
     },
     {
-        path: imageFiles[5],
+        path: [imageFiles[5], imageFilesMobile[5]],
         artist: 'ZER1',
         genre: 'Pixel Art',
     },
     {
-        path: imageFiles[6],
+        path: [imageFiles[6], imageFilesMobile[6]],
         artist: 'J1N',
         genre: 'Abstractism',
     },
     {
-        path: imageFiles[7],
+        path: [imageFiles[7], imageFilesMobile[7]],
         artist: 'MIRR0R',
         genre: 'Watercolor',
     },
     {
-        path: imageFiles[8],
+        path: [imageFiles[8], imageFilesMobile[8]],
         artist: 'PAUL0',
         genre: 'Expressionism',
     },
     {
-        path: imageFiles[9],
+        path: [imageFiles[9], imageFilesMobile[9]],
         artist: 'R1N',
         genre: 'Cyberpunk Art',
     },
@@ -109,7 +131,8 @@ function Default() {
                                     <div className="art-img-container">
                                         <img
                                             className="art-img"
-                                            src={art.path}
+                                            srcSet={`${art.path[1]} 300w, ${art.path[0]} 1000w`}
+                                            src={art.path[0]}
                                             alt={`${art.artist} 's artwork`}
                                         />
                                         <div className="genre-on-img">{art.genre}</div>
